0b7f947e47fea559ba70c684e73e6457a63f0c93
douglasbolis <douglasbolislima@gmail.com>
2015-02-24 13:54:21 -0300

    questao_1.py adicionado

---
+++ questao_1.py
@@ -0,0 +1,19 @@
+def f_criaFracao(n, d):
+	
+#fim funcao
+
+def main():
+	num, den = 0, 0
+	x = ()
+	
+	num = int(input("Informe um valor para o numerador: "))
+	den = int(input("Informe um valor para o denominador: "))
+	
+	x = f_criaFuncao(num, den)	
+	
+	return 0
+#fim main
+
+if __name__ == '__main__':
+	main()
+#fim if
